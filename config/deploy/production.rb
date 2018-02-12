3b5a3b332c54bdda85ade904180a4aeb7355a648
shreekrishna1726 <shreekrishna1726@gmail.com>
2018-02-13 00:47:44 +0530

    deploy setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '52.15.122.157', roles: [:web, :app, :db], primary: true
+server '52.15.122.157',user: 'stakeway', roles: [:web, :app, :db], primary: true
 set :rails_env, 'production'
 set :stage, 'production'
 set :rack_env, 'production'
