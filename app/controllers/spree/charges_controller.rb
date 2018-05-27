32385122be3ef00de3673d7e1cb6608c91c1651b
shreekrishna1726 <shreekrishna1726@gmail.com>
2018-05-27 13:55:04 +0530

    quick fixed

--- app/controllers/spree/charges_controller.rb
+++ app/controllers/spree/charges_controller.rb
@@ -3,49 +3,54 @@ module Spree
     before_action :authenticate_user!
     before_action :set_api
 
-    require 'Instamojo-rb'
-    require 'paypal-sdk-rest'
-    include PayPal::SDK::REST::DataTypes
-    include PayPal::SDK::Core::Logging
-    PayPal::SDK::REST.set_config(
-    :mode => "live", # "sandbox" or "live"
-    :client_id => "Aaewj1H855mxvvxqwD_0bHGGbPcLV4QfjRKCPgdameV929-Z7atuFTOv7bI1TeXx3OOszhFbApXBaDEx-UTcODg_kaTC0HnBQ0Zkm6kvQ1NZQwFYxWR2R_Bbis6VMy",
-    :client_secret => "EFBnNRCXZKobuKn9VEKpKdXAIGTWnoRlg6axJSGoiRvpWzNWLAAyq9c6gGig-dwQV52Rpj95PCixXbQb")
+    # require 'Instamojo-rb'
+    # require 'paypal-sdk-rest'
+    # include PayPal::SDK::REST::DataTypes
+    # include PayPal::SDK::Core::Logging
+    # PayPal::SDK::REST.set_config(
+    # :mode => "live", # "sandbox" or "live"
+    # :client_id => "Aaewj1H855mxvvxqwD_0bHGGbPcLV4QfjRKCPgdameV929-Z7atuFTOv7bI1TeXx3OOszhFbApXBaDEx-UTcODg_kaTC0HnBQ0Zkm6kvQ1NZQwFYxWR2R_Bbis6VMy",
+    # :client_secret => "EFBnNRCXZKobuKn9VEKpKdXAIGTWnoRlg6axJSGoiRvpWzNWLAAyq9c6gGig-dwQV52Rpj95PCixXbQb")
     
     # @@api = Instamojo::API.new("test_7a8227fcbc492750c81b5c7be5a", "test_0e624f8d7a4b979484533e38ded", "https://test.instamojo.com/api/1.1/")
     # @@api = Instamojo::API.new("test_7a8227fcbc492750c81b5c7be5a", "test_0e624f8d7a4b979484533e38ded", "https://test.instamojo.com/api/1.1/")
     def new
-      @payment = Payment.new({
-        :intent =>  "sale",
-        :payer =>  {
-          :payment_method =>  "paypal" },
-        :redirect_urls => {
-          :return_url => "http://localhost:3000/charges/paymentDetail",
-          :cancel_url => "http://localhost:3000/" },
-        :transactions =>  [{
-          :amount =>  {
-            :total =>  "150",
-            :currency =>  "INR" },
-          :description =>  "This is the payment transaction description." }]})
-      
-      if @payment.create
-        p"create"
-        redirect_to @payment.links[1].href
+      if current_spree_user.active
+        redirect_to '/profiles/new'
       else
-        p"error"
-        @payment.error 
+        @charge = Charge.new
       end
+      # @payment = Payment.new({
+      #   :intent =>  "sale",
+      #   :payer =>  {
+      #     :payment_method =>  "paypal" },
+      #   :redirect_urls => {
+      #     :return_url => "http://localhost:3000/charges/paymentDetail",
+      #     :cancel_url => "http://localhost:3000/" },
+      #   :transactions =>  [{
+      #     :amount =>  {
+      #       :total =>  "150",
+      #       :currency =>  "INR" },
+      #     :description =>  "This is the payment transaction description." }]})
+      
+      # if @payment.create
+      #   p"create"
+      #   redirect_to @payment.links[1].href
+      # else
+      #   p"error"
+      #   @payment.error 
+      # end
     end
 
 
     def paymentDetail
-        payment = Payment.find(params[:paymentId])
-        if payment.execute( :payer_id => params[:PayerID] )
-          current_spree_user.update_attributes(active: true)
-          redirect_to '/profiles/new'          
-        else
-          payment.error 
-        end
+        # payment = Payment.find(params[:paymentId])
+        # if payment.execute( :payer_id => params[:PayerID] )
+        #   current_spree_user.update_attributes(active: true)
+        #   redirect_to '/profiles/new'          
+        # else
+        #   payment.error 
+        # end
       # @status = @@api.client.payment_detail(params[:payment_id])
       # # p "-------------------------------"
       # p @status 
@@ -64,6 +69,16 @@ module Spree
       
     end
 
+
+    def create
+      @charge =  Charge.create(charge_param.merge(profile_id: current_spree_user.id))
+      if @charge.save
+        redirect_to '/profiles/new', notice: 'Thank you:)'
+      else
+        redirect_to '/charges/new', notice: 'Something went wrong(:'
+      end
+    end
+
     private
 
     def set_api
@@ -76,6 +91,11 @@ module Spree
       #   app.api_key = "e737b4ef3f19195217e591fc8ad3e057@instamojo.com"
       #   app.auth_token = "0c12707dd9d2a2bda1a4fd7893e83a9e@instamojo.com"
       # end
+    end
+
+
+    def charge_param
+      params.require(:charge).permit(:amount, :txn_number)
     end
 
 
--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,4 +1,15 @@
 class Charge < ApplicationRecord
-	belongs_to :profile ,class_name:"Profile"
+	belongs_to :user ,class_name:"Spree::User", foreign_key: :profile_id
+	after_create :update_user_active
+
+
+
+	private
+		def update_user_active
+			self.user.update(active:true);
+		end
 
 end
+
+
+
